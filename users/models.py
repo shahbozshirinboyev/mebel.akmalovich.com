bf0189f300963d78eeb561dd655a053d848fe858
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2026-02-16 18:51:41 +0500

    users table edit some section

--- users/models.py
+++ users/models.py
@@ -5,12 +5,17 @@ import uuid
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField( _("Телефон"), max_length=20, blank=True, null=True)
     is_worker = models.BooleanField(
-        _("worker"),
+        _("Работник"),
         default=False,
-        help_text=_("Yangi foydalanuvchi xodim yoki xodim emas."),
+        help_text=_("Отметьте, если пользователь является сотрудником."),
         )
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    full_name.short_description = 'Имя и Фамилия'
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.username}"
